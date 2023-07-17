480b38055826405b1fcf3ff3508e65d4e2cc10b3
Justin Brooks <jsbroks@gmail.com>
2023-07-17 14:45:29 -0400

    fix: Use deployer release channels

--- pkg/wandb/deployer/deployer.go
+++ pkg/wandb/deployer/deployer.go
@@ -3,5 +3,6 @@ package deployer
 import "github.com/wandb/operator/pkg/utils"
 
 var (
-	DeployerAPIUrl = utils.Getenv("DEPLOYER_API_URL", "https://localhost:3000/api")
+	DeployerAPIUrl        = utils.Getenv("DEPLOYER_API_URL", "https://localhost:3000/api")
+	DeployerLicenseHeader = utils.Getenv("DEPLOYER_LICENSE_HEADER", "x-wandb-license")
 )
