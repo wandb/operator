f0512dccb5fc2bb7010693541bc318f1a7f58aa7
Justin Brooks <jsbroks@gmail.com>
2023-08-09 19:36:49 -0400

    chore: cleanup

--- pkg/wandb/spec/spec.go
+++ pkg/wandb/spec/spec.go
@@ -9,18 +9,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// type Spec interface {
-// 	SetValues(map[string]interface{})
-// 	GetValues() map[string]interface{}
-// 	Apply(
-// 		context.Context,
-// 		client.Client,
-// 		*v1.WeightsAndBiases,
-// 		*runtime.Scheme,
-// 	) error
-// 	Merge(spec Spec) Spec
-// }
-
 type Release interface {
 	Apply(
 		context.Context,
@@ -69,12 +57,4 @@ func (s *Spec) Merge(spec *Spec) {
 	if spec.Config != nil {
 		s.MergeConfig(spec.Config)
 	}
-}
-
-func Merge(specs ...*Spec) *Spec {
-	root := &Spec{}
-	for _, spec := range specs {
-		root.Merge(spec)
-	}
-	return root
 }
