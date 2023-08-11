5a6c4c355b415ad3d182644ade95e69274da5b9e
Zachary Blasczyk <77289967+wandb-zacharyblasczyk@users.noreply.github.com>
2023-08-10 21:50:13 -0400

    feat: Add active-state cm (#2)

--- controllers/weightsandbiases_controller.go
+++ controllers/weightsandbiases_controller.go
@@ -139,10 +139,16 @@ func (r *WeightsAndBiasesReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	}
 	log.Info("Successfully applied spec", "spec", desiredSpec)
 
-	r.Recorder.Event(wandb, corev1.EventTypeNormal, "ConfigBackup", "Creating a copy of desired state")
+	r.Recorder.Event(wandb, corev1.EventTypeNormal, "SavingConfig", "Creating a copy of desired state")
 	if err := specManager.Backup(desiredSpec); err != nil {
 		r.Recorder.Event(wandb, corev1.EventTypeNormal, "BackupFailed", "Failed to backup desired state")
 		log.Error(err, "Failed to backup sucessful spec.")
+		return ctrlqueue.DoNotRequeue()
+	}
+
+	if err := specManager.SetActive(desiredSpec); err != nil {
+		r.Recorder.Event(wandb, corev1.EventTypeNormal, "SetActiveFailed", "Failed to save active state")
+		log.Error(err, "Failed to save active sucessful spec.")
 		return ctrlqueue.DoNotRequeue()
 	}
 
