356c11530feed8108b8631f1f4b0de0bf619c0ea
Aman Pruthi <amanpruthi404@gmail.com>
2025-02-13 13:29:04 +0530

    updated condition to retrieve the license from secret and then variable

--- pkg/wandb/spec/utils/license.go
+++ pkg/wandb/spec/utils/license.go
@@ -25,28 +25,35 @@ func GetLicense(specs ...*spec.Spec) string {
 			continue
 		}
 
+		secretName := s.Values.GetString("global.licenseSecret.name")
+		secretKey := s.Values.GetString("global.licenseSecret.key")
+		secretNamespace := s.Values.GetString("global.licenseSecret.namespace")
+		if secretNamespace == "" {
+			secretNamespace = "default"
+		}
+
+		if secretName != "" && secretKey != "" {
+			license := getLicenseFromSecret(kubeClient, secretName, secretKey, secretNamespace)
+			if license != "" {
+				log.Info("License retrieved from Kubernetes secret", "secretName", secretName, "namespace", secretNamespace)
+				return license
+			}
+		}
+
 		license := s.Values.GetString("global.license")
 		if license != "" {
 			log.Info("License retrieved from values.yaml")
 			return license
 		}
-
-		secretName := s.Values.GetString("global.licenseSecret.name")
-		secretKey := s.Values.GetString("global.licenseSecret.key")
-
-		if secretName != "" && secretKey != "" {
-			license := getLicenseFromSecret(kubeClient, secretName, secretKey)
-			if license != "" {
-				log.Info("License retrieved from Kubernetes secret", "secretName", secretName)
-				return license
-			}
-		}
 	}
-	return "" 
+	return ""
 }
 
 func createKubeClient() (client.Client, error) {
-	kubeConfig := config.GetConfigOrDie()
+	kubeConfig, err := config.GetConfig()
+	if err != nil {
+		return nil, err
+	}
 	kubeClient, err := client.New(kubeConfig, client.Options{})
 	if err != nil {
 		return nil, err
@@ -54,26 +61,26 @@ func createKubeClient() (client.Client, error) {
 	return kubeClient, nil
 }
 
-func getLicenseFromSecret(kubeClient client.Client, secretName, secretKey string) string {
+func getLicenseFromSecret(kubeClient client.Client, secretName, secretKey, secretNamespace string) string {
 	log := ctrllog.Log.WithName("getLicenseFromSecret")
 	secret := &corev1.Secret{}
 	ctx := context.TODO()
 
-	err := kubeClient.Get(ctx, client.ObjectKey{Name: secretName, Namespace: "default"}, secret)
+	err := kubeClient.Get(ctx, client.ObjectKey{Name: secretName, Namespace: secretNamespace}, secret)
 	if err != nil {
-		log.Error(err, "Error retrieving secret", "secretName", secretName)
+		log.Error(err, "Error retrieving secret", "secretName", secretName, "namespace", secretNamespace)
 		return ""
 	}
 
 	if secret.Data == nil {
-		log.Info("Secret has no data", "secretName", secretName)
+		log.Info("Secret has no data", "secretName", secretName, "namespace", secretNamespace)
 		return ""
 	}
 	license, exists := secret.Data[secretKey]
 	if !exists {
-		log.Info("Key not found in secret", "secretKey", secretKey, "secretName", secretName)
+		log.Info("Key not found in secret", "secretKey", secretKey, "secretName", secretName, "namespace", secretNamespace)
 		return ""
 	}
-	log.Info("Successfully retrieved license from secret", "secretName", secretName)
+	log.Info("Successfully retrieved license from secret", "secretName", secretName, "namespace", secretNamespace)
 	return string(license)
 }
