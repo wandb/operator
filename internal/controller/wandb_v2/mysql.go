194b87adb08294f6434dd7af504e5a2bc58e68fc
jonathan meeks <jonathan.meeks@wandb.com>
2025-10-18 11:07:08 -0500

    refactor

--- internal/controller/wandb_v2/mysql.go
+++ internal/controller/wandb_v2/mysql.go
@@ -2,47 +2,128 @@ package wandb_v2
 
 import (
 	"context"
+	"errors"
 
 	ndbv1 "github.com/mysql/ndb-operator/pkg/apis/ndbcontroller/v1"
 	apiv2 "github.com/wandb/operator/api/v2"
-	"k8s.io/apimachinery/pkg/api/errors"
+	machErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
+
+type wandbNdkMysqlWrapper struct {
+	installed bool
+	obj       *ndbv1.NdbCluster
+}
+
+type wandbNdkMysqlDoReconcile interface {
+	Execute(ctx context.Context, r *WeightsAndBiasesV2Reconciler) (ctrl.Result, error)
+}
+
+func mysqlNamespacedName(req ctrl.Request) types.NamespacedName {
+	return types.NamespacedName{
+		Name:      "wandb-mysql",
+		Namespace: req.Namespace,
+	}
+}
 
 func (r *WeightsAndBiasesV2Reconciler) handleMysql(
 	ctx context.Context, wandb *apiv2.WeightsAndBiases, req ctrl.Request,
 ) (
 	ctrl.Result, error,
 ) {
-	mysqlNamespacedName := types.NamespacedName{
-		Name:      "wandb-mysql",
-		Namespace: req.Namespace,
+	var err error
+	var desiredMysql wandbNdkMysqlWrapper
+	var actualMysql wandbNdkMysqlWrapper
+	var reconciliation wandbNdkMysqlDoReconcile
+
+	if actualMysql, err = r.actualNdkMysql(ctx, req); err != nil {
+		return ctrl.Result{}, err
 	}
+	if desiredMysql, err = r.desiredNdkMysql(ctx, req, wandb); err != nil {
+		return ctrl.Result{}, err
 
-	var expectMysql bool
-	var hasMysql bool
+	}
+	if reconciliation, err = computeReconcileDrift(ctx, desiredMysql, actualMysql); err != nil {
+		return ctrl.Result{}, err
+	}
+	if reconciliation != nil {
+		return reconciliation.Execute(ctx, r)
+	}
+	return ctrl.Result{}, nil
+}
 
-	expectMysql = wandb.Spec.Database.Enabled
+// groupVersionKind may be necessary if there is a case where WeightsAndBiases object is missing GroupVersionKind
+func (r *WeightsAndBiasesV2Reconciler) groupVersionKind(wandb *apiv2.WeightsAndBiases) (schema.GroupVersionKind, error) {
+	gvks, _, err := r.Scheme.ObjectKinds(wandb)
+	if err != nil || len(gvks) == 0 {
+		return schema.GroupVersionKind{}, err
+	}
+	if len(gvks) == 0 {
+		return schema.GroupVersionKind{}, errors.New("no GroupKindVersion for WeightsAndBiases Scheme")
+	}
+	gvk := gvks[0]
+	return gvk, nil
+}
 
-	existingMysql := &ndbv1.NdbCluster{}
-	err := r.Get(ctx, mysqlNamespacedName, existingMysql)
+func (r *WeightsAndBiasesV2Reconciler) actualNdkMysql(
+	ctx context.Context, req ctrl.Request,
+) (
+	wandbNdkMysqlWrapper, error,
+) {
+	result := wandbNdkMysqlWrapper{
+		installed: false,
+		obj:       nil,
+	}
+	obj := &ndbv1.NdbCluster{}
+	err := r.Get(ctx, mysqlNamespacedName(req), obj)
 	if err != nil {
-		if errors.IsNotFound(err) {
-			hasMysql = false
+		if machErrors.IsNotFound(err) {
+			return result, nil
 		} else {
-			return ctrl.Result{}, err
+			return result, err
 		}
 	} else {
-		hasMysql = true
+		result.obj = obj
+		result.installed = true
 	}
+	return result, nil
+}
 
-	if expectMysql && !hasMysql {
-		newMysql := &ndbv1.NdbCluster{
+func (r *WeightsAndBiasesV2Reconciler) desiredNdkMysql(
+	ctx context.Context, req ctrl.Request, wandb *apiv2.WeightsAndBiases,
+) (
+	wandbNdkMysqlWrapper, error,
+) {
+	result := wandbNdkMysqlWrapper{
+		installed: false,
+		obj:       nil,
+	}
+	if wandb.Spec.Database.Enabled {
+		result.installed = true
+		mysqlNamespacedName := mysqlNamespacedName(req)
+		gvk := wandb.GroupVersionKind()
+		if gvk.GroupVersion().Group == "" || gvk.GroupVersion().Version == "" || gvk.Kind == "" {
+			return result, errors.New("no GroupKindVersion for WeightsAndBiases CR")
+		}
+		blockOwnerDeletion := true
+		isController := true
+		result.obj = &ndbv1.NdbCluster{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      mysqlNamespacedName.Name,
 				Namespace: mysqlNamespacedName.Namespace,
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion:         gvk.GroupVersion().String(),
+						Kind:               gvk.Kind,
+						Name:               wandb.GetName(),
+						UID:                wandb.GetUID(),
+						BlockOwnerDeletion: &blockOwnerDeletion,
+						Controller:         &isController,
+					},
+				},
 			},
 			Spec: ndbv1.NdbClusterSpec{
 				DataNode: &ndbv1.NdbDataNodeSpec{
@@ -50,46 +131,50 @@ func (r *WeightsAndBiasesV2Reconciler) handleMysql(
 				},
 			},
 		}
+	}
+	return result, nil
+}
 
-		gvks, _, err := r.Scheme.ObjectKinds(wandb)
-		if err != nil || len(gvks) == 0 {
-			return ctrl.Result{}, err
-		}
-		gvk := gvks[0]
+func computeReconcileDrift(
+	ctx context.Context, desiredMysql, actualMysql wandbNdkMysqlWrapper,
+) (
+	wandbNdkMysqlDoReconcile, error,
+) {
+	if (!desiredMysql.installed) && (actualMysql.installed) {
+		return &wandbNdkMysqlDelete{
+			actual: actualMysql,
+		}, nil
+	}
+	if (desiredMysql.installed) && (!actualMysql.installed) {
+		return &wandbNdkMysqlCreate{
+			desired: desiredMysql,
+		}, nil
+	}
+	return nil, nil
+}
 
-		blockOwnerDeletion := true
-		isController := true
-		newMysql.ObjectMeta.OwnerReferences = append(
-			newMysql.ObjectMeta.OwnerReferences,
-			metav1.OwnerReference{
-				APIVersion:         gvk.GroupVersion().String(),
-				Kind:               gvk.Kind,
-				Name:               wandb.GetName(),
-				UID:                wandb.GetUID(),
-				BlockOwnerDeletion: &blockOwnerDeletion,
-				Controller:         &isController,
-			},
-		)
+type wandbNdkMysqlCreate struct {
+	desired wandbNdkMysqlWrapper
+}
 
-		if err := r.Create(ctx, newMysql); err != nil {
-			return ctrl.Result{}, err
-		}
-	} else if !expectMysql && hasMysql {
-		// Check if this NdbCluster is owned by this WeightsAndBiases CR
-		// Only delete if it's owned by us to avoid deleting manually created clusters
-		isOwnedByUs := false
-		for _, owner := range existingMysql.ObjectMeta.OwnerReferences {
-			if owner.UID == wandb.GetUID() {
-				isOwnedByUs = true
-				break
-			}
-		}
+func (c *wandbNdkMysqlCreate) Execute(ctx context.Context, r *WeightsAndBiasesV2Reconciler) (ctrl.Result, error) {
+	log := ctrl.LoggerFrom(ctx)
+	log.Info("Installing NDK Mysql")
+	if err := r.Create(ctx, c.desired.obj); err != nil {
+		return ctrl.Result{}, err
+	}
+	return ctrl.Result{}, nil
+}
 
-		if isOwnedByUs {
-			if err := r.Delete(ctx, existingMysql); err != nil {
-				return ctrl.Result{}, err
-			}
-		}
+type wandbNdkMysqlDelete struct {
+	actual wandbNdkMysqlWrapper
+}
+
+func (d *wandbNdkMysqlDelete) Execute(ctx context.Context, r *WeightsAndBiasesV2Reconciler) (ctrl.Result, error) {
+	log := ctrl.LoggerFrom(ctx)
+	log.Info("Uninstalling NDK Mysql")
+	if err := r.Delete(ctx, d.actual.obj); err != nil {
+		return ctrl.Result{}, err
 	}
 	return ctrl.Result{}, nil
 }
