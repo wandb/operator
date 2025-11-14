73e2c914feeaa29c200986d9f45f1b9b630b7e83
jonathan meeks <jonathan.meeks@wandb.com>
2025-11-14 13:25:15 -0600

    kafka refactor, phase 3

---
+++ internal/controller/wandb_v2/kafka.go
@@ -0,0 +1,49 @@
+package wandb_v2
+
+import (
+	"context"
+
+	apiv2 "github.com/wandb/operator/api/v2"
+	"github.com/wandb/operator/internal/controller/infra/kafka"
+	"github.com/wandb/operator/internal/controller/infra/kafka/strimzi"
+	"github.com/wandb/operator/internal/model"
+)
+
+func (r *WeightsAndBiasesV2Reconciler) reconcileKafka(
+	ctx context.Context,
+	infraDetails model.InfraConfig,
+	wandb *apiv2.WeightsAndBiases,
+) *model.Results {
+	var err error
+	var results = &model.Results{}
+	var nextResults *model.Results
+	var kafkaConfig model.KafkaConfig
+	var actual kafka.ActualKafka
+
+	if kafkaConfig, err = infraDetails.GetKafkaConfig(); err != nil {
+		results.AddErrors(err)
+		return results
+	}
+
+	if actual, err = strimzi.Initialize(ctx, r.Client, kafkaConfig, wandb, r.Scheme); err != nil {
+		results.AddErrors(err)
+		return results
+	}
+
+	if kafkaConfig.Enabled {
+		nextResults = actual.Upsert(ctx, kafkaConfig)
+	} else {
+		nextResults = actual.Delete(ctx)
+	}
+	results.Merge(nextResults)
+	if results.HasCriticalError() {
+		return results
+	}
+
+	wandb.Status.KafkaStatus = results.ExtractKafkaStatus(ctx)
+	if err = r.Status().Update(ctx, wandb); err != nil {
+		results.AddErrors(err)
+	}
+
+	return results
+}
